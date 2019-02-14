c209862b00abc08777cd31e4c24094d61a9d09cc
hyw123 <1274418183@qq.com>
2019-02-14 22:09:59 +0800

    添加UI列表代码

---
+++ Rich Engine/Assets/Scripts/UI/ListItemUI.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ListItemUI : MonoBehaviour
+{
+
+	private Text Text_Name;
+	private Text Text_Day;
+	private Text Text_Content;
+	private Text Text_Title;
+	private Image Image_RedPoint;
+	private Image Image_flag;
+
+	public GameObject inbox_email;
+
+	private int m_flag = -1;
+
+	public int m_index = -1;
+	void Awake()
+	{
+		Text_Name = transform.Find ("txt_No").GetComponent<Text>();
+		//Text_Day = transform.Find ("Text_Day").GetComponent<Text>();
+		//Text_Content = transform.Find ("Text_Content").GetComponent<Text>();
+		//Text_Title = transform.Find ("Text_Title").GetComponent<Text>();
+		//Image_RedPoint = transform.Find ("Image_RedPoint").GetComponent<Image>();
+		//Image_flag = transform.Find ("Image_flag").GetComponent<Image>();
+	}
+
+    public void SetItemData(GameObject item, int index)
+    {
+        //itemData = item;
+        m_index = index;
+
+
+        Text_Name.text = index.ToString();
+
+        ////		string content = line_email ["Content"].ToString ();
+        //int messageID = int.Parse(line_email["MessageID"].ToString());
+        //Debug.Log("messageID  " + messageID);
+        //string content = LoadJson.GetLine(messageID, GameControl.instance.table_message)["Condition"].ToString();
+        //Debug.Log("content  " + content);
+
+        //Text_Content.text = content;
+        //Text_Day.text = GameControl.instance.getGameTime(itemData.time);
+        //Text_Title.text = line_email["Title"].ToString();
+
+
+
+    }
+}
---
+++ Rich Engine/Assets/Scripts/UI/ListScroll.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class ListScroll : MonoBehaviour {
+	public List<GameObject> items = new List<GameObject>();
+
+	private int mCount = 0;
+
+	private ArrayList mList = new ArrayList();
+	private string m_personID = "-1";
+
+	// Use this for initialization
+	void Start () {
+        mCount = 0;
+        for (int i = 0; i < 12; i++)
+        {
+            mList.Add(i);
+
+        }
+    }
+
+    void Update()
+    {
+        if (mCount < mList.Count)
+        {
+            //根据item数量改变滚动区域的大小
+            int count = mList.Count;
+            transform.GetComponent<RectTransform>().sizeDelta = new Vector2(0, 200 * count);
+            for (int i = count - 1; i >= 0; i--)
+            {
+                if (count - i > 10)
+                {
+                    break;
+                }
+                items[10 - count + i].SetActive(true);
+                items[10 - count + i].GetComponent<ListItemUI>().SetItemData(items[0], i);
+            }
+
+            mCount = count;
+            Debug.Log("email count  " + mCount);
+        }
+
+        if (mList.Count > 10)
+        {
+            if (items[0].transform.position.y >= -100)//上滑
+            {
+                int index = items[0].GetComponent<ListItemUI>().m_index - 1;//首先判断是否为第一封，是的话就表示显示完了，不需要换位置了
+                if (index < 0)
+                {
+                    return;
+                }
+
+                Vector3 pos = items[0].transform.localPosition;//获得还没有移动位置之前第一个元素的位置
+
+                items.Insert(0, items[9]);//将最后的元素移动到最前面
+
+                items[0].transform.localPosition = new Vector3(pos.x, pos.y - 200, pos.z);//重新设置移动位置之后第一个元素的位置
+
+                items.RemoveAt(items.Count - 1);//将之前最后元素删除
+
+                //Emails email = m_inBoxList[index] as Emails;
+                items[0].GetComponent<ListItemUI>().SetItemData(items[0], index);
+            }
+            if (items[0].transform.position.y <= -100)//下滑
+            {
+                int index = items[items.Count - 1].GetComponent<ListItemUI>().m_index + 1;//首先判断是否为最后，是的话就表示显示完了，不需要换位置了
+                if (index >= mList.Count)
+                {
+                    return;
+                }
+
+                Vector3 pos = items[items.Count - 1].transform.localPosition;//获得还没有移动位置之前最后一个元素的位置
+
+                items.Add(items[0]);//将最前的元素移动到最后面
+
+                items[items.Count - 1].transform.localPosition = new Vector3(pos.x, pos.y + 200, pos.z);//重新设置移动位置之后最后一个元素的位置
+
+                items.RemoveAt(0);//将之前最前面的元素删除；
+
+                //Emails email = m_inBoxList[index] as Emails;
+                items[items.Count - 1].GetComponent<ListItemUI>().SetItemData(items[0], index);
+            }
+        }
+    }
+
+
+    void DeleteItem(Transform item)
+	{
+        mList.Remove (item.gameObject);
+		Destroy (item.gameObject);
+
+		int count = mList.Count;
+		transform.GetComponent<RectTransform> ().sizeDelta = new Vector2(0, 200*count);
+	}
+}
+
